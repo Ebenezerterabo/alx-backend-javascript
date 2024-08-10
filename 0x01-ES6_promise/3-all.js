cd137ea4cd4dd88c41a0f0306d0fe971e76b9365
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-10 02:50:02 +0000

    Task 3 solved

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  const allPromises = Promise.all([uploadPhoto(), createUser()]);
+  allPromises
+    .then((results) => {
+      // Destrucuring the array results
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    });
+}
