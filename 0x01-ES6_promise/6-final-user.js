2638a1356e531a07b91f22dfe6bd3cfc50606016
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-11 05:58:20 +0000

    Task 6 solved

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstname, lastname, filename) {
   const promiseSettled = Promise.allSettled([signUpUser(firstname, lastname),
     uploadPhoto(filename)]);
-  return promiseSettled;
+  return promiseSettled
+    .then((results) => {
+      if (results.status === 'fulfilled') {
+        return { status: results.status, value: results.value };
+      }
+      return { status: results.status, value: results.reason };
+    });
 }
