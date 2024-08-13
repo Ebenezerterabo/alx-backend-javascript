cdcb9eb4cf9b2a8c44e603c0152df76975052f95
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-13 13:59:08 +0000

    Task 4 solved

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency'; // eslint-disable-line no-unused-vars
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and Setter (amount)
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter and Setter (currency)
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Display full price method
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  // Static method
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
