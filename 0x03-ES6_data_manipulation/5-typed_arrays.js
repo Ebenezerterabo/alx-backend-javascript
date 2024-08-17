f001bdfa03b936f83c20f33916e7fab52c2cc4a1
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-17 02:46:04 +0000

    Task 5 solved

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,6 +4,9 @@ export default function createInt8TypedArray(length, position, value) {
   // Create a new view
   const dataView = new DataView(buffer);
   // set the value
+  if (position >= length || position < 0) {
+    throw new Error('Position outside the array');
+  }
   dataView[position] = value;
   dataView.setInt8(position, value);
 
