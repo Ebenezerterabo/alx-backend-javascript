e41179da3e0c0082c1bd633526c7451286ce0234
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-10-03 01:59:56 +0000

    Task 0 solved

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,9 @@
 /* Function to calculate number */
 function calculateNumber(a, b) {
-    sum = Math.round(a) + Math.round(b);
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('parameters must be numbers');
+    }
+    const sum = Math.round(a) + Math.round(b);
     return (sum);
 }
 
